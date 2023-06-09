5a22c9e944071daed7830df1134e6f63bd8fb269
Shin <shin4895@gmail.com>
2023-06-09 19:16:49 +0900

    리팩토링
    비동기에러처리 , 로딩인디케이터 구현

--- src/components/uploadpdf.js
+++ src/components/uploadpdf.js
@@ -1,33 +1,49 @@
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
 import { convertPdf, getJob } from "../utils/fetchapis";
 import Addquestion from "./addquestion";
 import { uploadResume } from "../pages/api/firebase";
 import { useAuthContext } from "@/pages/_app";
 
+const FileInput = ({ setSelectedFile }) => (
+  <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
+);
+
 export default function UploadPDF() {
-  const { user } = useAuthContext();
+  const { uid } = useAuthContext();
   const [selectedFile, setSelectedFile] = useState();
   const [resultConvert, setResultConvert] = useState();
   const [resultJob, setResultJob] = useState();
-  const submit = async (e) => {
-    e.preventDefault();
-    const resultConvert = await convertPdf(selectedFile);
-    setResultConvert(resultConvert);
-    const resultJob = await getJob(resultConvert);
-    setResultJob(resultJob);
-    const resume = resultConvert.toString();
-    await uploadResume(resume, user.uid);
-    console.log(user.uid, resume);
+  const [loading, setLoading] = useState(false);
+
+  const uploadAndConvertFile = async (file) => {
+    try {
+      setLoading(true);
+      const resultConvert = await convertPdf(file);
+      setResultConvert(resultConvert);
+      const resultJob = await getJob(resultConvert);
+      setResultJob(resultJob);
+      const resume = resultConvert.toString();
+      await uploadResume(resume, uid);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   };
+
+  useEffect(() => {
+    if (selectedFile) {
+      uploadAndConvertFile(selectedFile);
+    }
+  }, [selectedFile]);
+
   return (
     <div>
-      <input type="file" onChange={(e) => setSelectedFile(e.target.files[0])} />
-      <button onClick={submit}>Upload</button>
-      <Addquestion
-        resultConvert={resultConvert}
-        resultJob={resultJob}
-      ></Addquestion>
+      <FileInput setSelectedFile={setSelectedFile} />
+      {loading && <div>Uploading and converting...</div>}
+      {resultConvert && resultJob && (
+        <Addquestion resultConvert={resultConvert} resultJob={resultJob} />
+      )}
     </div>
   );
 }
