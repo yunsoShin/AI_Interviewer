f809adb35d9f787fb6c719069226db84fabd9f5b
Shin <shin4895@gmail.com>
2023-06-11 23:12:34 +0900

    CSS

--- src/components/answer.js
+++ src/components/answer.js
@@ -14,11 +14,11 @@ function Answer() {
             value={answer}
             onChange={(e) => setAnswer(e.target.value)}
             rows={4}
-            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black mt-5"
             placeholder={"This is conducted/progressed by chain reactions."}
           />
           <button
-            className="bg-black rounded-xl text-white font-medium px-4 sm:mt-10 mt-2 hover:bg-black/80 w-full"
+            className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
             onClick={(e) => setPrompt(`${answer}`)}
           >
             답변하기 &rarr;
