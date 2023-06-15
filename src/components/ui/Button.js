e27a103a103f0b05c112e3822d4d004703692e81
Shin <shin4895@gmail.com>
2023-06-15 23:38:58 +0900

    Update Button.js

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -5,6 +5,7 @@ export default function Button({ text, onClick }) {
     <button
       className=" text-white py-3 px-5 rounded-md hover:brightness-150 mr-1 text-xs scale-90 md:scale-125 md:mr-10  bg-black"
       onClick={onClick}
+      id="buttonComponent"
     >
       {text}
     </button>
