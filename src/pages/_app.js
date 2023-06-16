709d61d2c056e5d44de57f4100117f178dfc770b
Shin <shin4895@gmail.com>
2023-06-17 00:19:54 +0900

    필요없는 주석삭제

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,3 @@
-// 필요한 모듈을 불러옵니다.
 import { createContext, useContext, useEffect, useState } from "react";
 import { login, logout, onUserStateChange } from "../pages/api/firebase";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -34,11 +33,8 @@ export function useAIProcess() {
   return useContext(AIProcessContext);
 }
 
-// createContext를 사용하여 새로운 Context를 생성합니다. 이 Context는 전역 상태를 공유하기 위해 사용됩니다.
 const AuthContext = createContext();
 
-// AuthContextProvider 컴포넌트를 정의합니다. 이 컴포넌트는 children props를 받아서 AuthContext.Provider 컴포넌트를 반환합니다.
-// 이 컴포넌트를 통해 value로 전달한 값들을 하위 컴포넌트들이 사용할 수 있게 됩니다.
 export function AuthContextProvider({ children }) {
   // useState 훅을 사용하여 user 상태를 관리합니다.
   const [user, setUser] = useState();
@@ -60,14 +56,10 @@ export function AuthContextProvider({ children }) {
   );
 }
 
-// useAuthContext라는 커스텀 훅을 정의합니다. 이 훅은 useContext 훅을 사용하여 AuthContext의 값을 가져옵니다.
-// 이 훅을 사용하면 컴포넌트 내에서 AuthContext의 값을 쉽게 사용할 수 있게 됩니다.
 export function useAuthContext() {
   return useContext(AuthContext);
 }
 
-// _app.js 파일에서 AuthContextProvider 컴포넌트를 최상위에 위치시킵니다.
-// 이렇게 하면 이 앱의 모든 컴포넌트에서 AuthContext의 값을 사용할 수 있게 됩니다.
 const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }) {
