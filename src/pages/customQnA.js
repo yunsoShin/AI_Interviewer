cc4a0fa07651c5abc0355aa35bb5d518eabd8ad7
Shin <shin4895@gmail.com>
2023-06-18 13:29:56 +0900

    커스텀QnA page 중간완성
    답변을 하고 해당 답변에 대한 상세 질문을 이어갈 수 있습니다.

--- src/pages/customQnA.js
+++ src/pages/customQnA.js
@@ -1,11 +1,39 @@
 import React from "react";
 import Navbar from "@/components/navbar";
-
-function CustomQnA(props) {
+import Answer from "@/components/answer";
+import Addquestion from "@/components/addquestion";
+import Head from "next/head";
+import Button from "@/components/ui/Button";
+import { useAIProcess } from "./_app";
+import { useFetchAndParse } from "@/hooks/useFetchAndParse";
+function CustomQnA() {
+  const { prompt, content } = useAIProcess();
+  const { loading, generatedBios, setGeneratedBios, setLoading } =
+    useFetchAndParse(content);
   return (
-    <div>
-      <Navbar></Navbar>
-    </div>
+    <>
+      <Head>
+        <title>AI Custom Interviewer</title>
+        <meta
+          name="description"
+          content="AI interview solution. It's made with openAI API"
+        />
+      </Head>
+      <div className=" pointer-events-auto">
+        <Navbar></Navbar>
+        <div>
+          <h2 className="md:text-3xl text-2xl font-bold text-slate-900 mx-auto text-center">
+            Custom AI interviewer
+          </h2>
+        </div>
+
+        <main className="mx-auto flex flex-col justify-center items-center  p-5">
+          <Addquestion generatedBios={generatedBios}></Addquestion>
+          {prompt}
+          <Answer></Answer>
+        </main>
+      </div>
+    </>
   );
 }
 
