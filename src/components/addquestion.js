272c10a06eb249bf37b7873deeaeb354ac15d009
Shin <shin4895@gmail.com>
2023-06-20 09:25:46 +0900

    Update addquestion.js

--- src/components/addquestion.js
+++ src/components/addquestion.js
@@ -24,49 +24,45 @@ function Addquestion({ generatedBios }) {
   }, [generatedBios]);
   return (
     <div className="w-full">
-      <>
-        <Toaster
-          position="top-center"
-          reverseOrder={false}
-          toastOptions={{ duration: 2000 }}
-        />
-
-        <div className="">
-          {generatedBios && (
-            <>
-              <hr className="bg-gray-700 border-1 dark:bg-gray-700" />
-              <div
-                className="space-y-8 flex flex-col items-center justify-center overflow-y-scroll h-[550px]  mr-10 md:translate-x-12 md:text-lg text-xs  translate-x-7"
-                ref={scrollRef}
-              >
-                {generatedBios
-                  .replaceAll("/", "")
-                  .trim()
-                  .split(/\d\./)
-                  .filter((generatedBio) => generatedBio.trim() !== "")
-                  .map((generatedBio, index) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border md:mr-10 mr-7"
-                        onClick={async () => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast("클립보드와 MyBox에 저장하였습니다", {
-                            icon: "✂️",
-                          });
-                          setLikes(generatedBio, uid);
-                        }}
-                        key={index}
-                      >
-                        <p>{prompt}</p>
-                        <p>{generatedBio}</p>
-                      </div>
-                    );
-                  })}
-              </div>
-            </>
-          )}
-        </div>
-      </>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+        toastOptions={{ duration: 2000 }}
+      />
+      <div className="">
+        {generatedBios && (
+          <>
+            <hr className="bg-gray-700 border-1 dark:bg-gray-700" />
+            <div
+              className="space-y-8 flex flex-col items-center justify-center overflow-y-scroll h-[550px]  mr-10 md:translate-x-12 md:text-lg text-xs  translate-x-7"
+              ref={scrollRef}
+            >
+              {generatedBios
+                .replaceAll("/", "")
+                .trim()
+                .split(/\d\./)
+                .filter((generatedBio) => generatedBio.trim() !== "")
+                .map((generatedBio, index) => {
+                  return (
+                    <div
+                      className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border md:mr-10 mr-7"
+                      onClick={async () => {
+                        navigator.clipboard.writeText(generatedBio);
+                        toast("클립보드와 MyBox에 저장하였습니다", {
+                          icon: "✂️",
+                        });
+                        setLikes(generatedBio, uid);
+                      }}
+                      key={index}
+                    >
+                      <p>{generatedBio}</p>
+                    </div>
+                  );
+                })}
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
